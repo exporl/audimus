7ea19f8468a3d1b95ed8d57e4c5d66dd207bdc5f
AnnaES <aschnell@gmx.net>
2017-09-13 11:57:24 +0200

    Working vocoder in R

--- vocoder.R
+++ vocoder.R
@@ -1,5 +1,11 @@
+### ------------------------------------------------------- ###
+### Clear objects from workspace
+### ------------------------------------------------------- ###
+remove(list=ls())
+
 library(signal)
-library(memisc)
+#library(memisc)
+
 vocoder <- function(d,fs,nbands){
   if(missing(fs)){fs <- 44100}
   if(missing(nbands)){nbands <- 8}
@@ -12,16 +18,24 @@ vocoder <- function(d,fs,nbands){
   
   #Create analysis filter bank
   filter_order <- 4
-  analysis_filters_B <- zeros(filter_order*2+1,nbands)
+  analysis_filters_B <- matrix(0, filter_order*2+1, nbands)
   analysis_filters_A <- analysis_filters_B
   
-  for(i in 1:nbands){
-    list[analysis_filters_B(,i), analysis_filters_A(,i)] <- butter(filter_order, (analysis_cutoff(i)+analysis_cutoff(i+1))/fs*2)
-  }	
   
-  if(doplot){
-    plot_filterbank(analysis_filters_B, analysis_filters_A)
-    title("Analysis filters")
+  for(i in 1:nbands){
+    critical_freqs <- c(analysis_cutoff[i], analysis_cutoff[i+1])/fs*2
+    #list(analysis_filters_B[,i], analysis_filters_A[,i]) <- butter(filter_order, critical_freqs)
+    results <- butter(filter_order, critical_freqs)
+    analysis_filters_B[,i] <- results$b
+    analysis_filters_A[,i] <- results$a
+    #butter(filter_order, critical_freqs)
+  }	
+  #analysis_filters_B <- a
+  #analysis_filters_A <- b
+  
+  if(doplot == 0){
+    plot_filterbank(analysis_filters_B, analysis_filters_A, "Analysis filters")
+    #title("Analysis filters")
   }
   
   #Create synthesis filter bank
@@ -33,42 +47,69 @@ vocoder <- function(d,fs,nbands){
   #[resynthesis_filters_B(:,i), resynthesis_filters_A(:,i)] <- butter(filter_order, [resynthesis_cutoff(i) resynthesis_cutoff(i+1)]/fs*2)
   #}
   
-  if(doplot){
-    plot_filterbank(resynthesis_filters_B, resynthesis_filters_A)
-    title("Resynthesis filters")
+  if(doplot == 0){
+    plot_filterbank(resynthesis_filters_B, resynthesis_filters_A, "Resynthesis filters")
+    #title("Resynthesis filters")
   }
   
   #Create L{ filter for envelope detection}
-  list[Blp, Alp] <- butter(4,50/fs*2)
+  #list[Blp, Alp] <- butter(4,50/fs*2)
+  resultsL <- butter(4,50/fs*2)
+  Blp <- resultsL$b
+  Alp <- resultsL$a
   
-  #Proess input signal
-  out <- zeros(length(d), nbands)
-  noise <- randn(lenngth(d),1)
+  #Process input signal
+  #out <- zeros(length(d), nbands)
+  out <- matrix(0, length(d), nbands)
+  
+  noise <- rnorm(length(d),0,1)
+  #cat("Length noise: ", length(noise), "\n")
+  #t <- NULL
   for(i in 1:nbands){
     #Filter input signal
-    t <- filter(analysis_filters_B(,i), analysis_filters_A(, i), d)
+    t <- filter(analysis_filters_B[,i], analysis_filters_A[,i], d)
     
     #Envelope detection
     t <- (t+abs(t))/2
     t <- filter(Blp, Alp, t)
+    #cat("Length t: ", length(t), "\n")
     
     #Create noise band
-    noiseband <- filter(resynthesis_filters_B(,i), resynthesis_filters_A(,i), noise)
-    out(,i) <- t.*noiseband		
+    noiseband <- filter(resynthesis_filters_B[,i], resynthesis_filters_A[,i], noise)
+    #cat("Length noiseband: ", length(noiseband), "\n")
+    testVar <- t[1:length(t)]*noiseband[1:length(noiseband)]
+    #cat("Length testVar: ", length(testVar), "\n")
+    #cat("Length out[,i]: ", length(out[,i]), "\n")
+    #cat("NAs in testVar?: ", any(is.na(testVar)), "\n")
+    
+    #cat("Head t", head(t), "\n")
+    #cat("Head noiseband", head(noiseband), "\n")
+    #cat("What is t: ", str(t), "\n")
+    #cat("What is noiseband: ", str(noiseband), "\n")
+    #cat("Length first 10: ", length(t[1:18749440]*noiseband[1:18749440]), "\n")
+    #cat("what's happening???", any(is.na(t)), "\n")
+    #cat("is here an NA?", any(is.na(noiseband)), "\n")
+    out[,i] <- testVar
   }
   
   r <- sum(out,2)
+  return(r)
 }
 
 
-plot_filterbank <- function(B,A){
-  for(i in 1:size(B,2)){
-   list[H,F] <- freqz(B(,i), A(,i), Fs = 44100)
-    semilogx(F, 20*log10(abs(H)))
+plot_filterbank <- function(B,A, titleName){
+  for(i in 1:ncol(B)){
+   #list[H,F] <- freqz(B[,i], A[,i], Fs = 44100)
+    resultsPlot <- freqz(B[,i], A[,i], Fs = 44100)
+    h <- resultsPlot$h
+    f <- resultsPlot$f
+    #semilogx(F, 20*log10(abs(H)))
+    #plot(f,20*log10(abs(h)), log="x", main = titleName, xlab = "Frequency", ylab = "Magnitude")
+    plot(xy.coords(f, 20*log10(abs(h)), log="x"), log="x", main = titleName, xlab = "Frequency", ylab = "Magnitude")
   }
-  xlabel("Freqeuncy")
-  ylabel("Magnitude")
-  set(gca, 'XScale', 'log')
+  #xlabel("Freqeuncy")
+  #ylabel("Magnitude")
+  #set(gca, 'XScale', 'log')
   
 }
 
@@ -90,10 +131,11 @@ analysis_cutoff_freqs_cochlear <- function(nbands, type){
     cum_num_bins <- c(1.5, 1.5 + cumsum(p.band_bins))
     p.crossover_freqs <- cum_num_bins * p.bin_freq
     p.band_widths <- diff(p.crossover_freqs)
-    p.char_freqs <- p.crossover_freqs(1:p.num_bands) + p.band_widths/2
+    p.char_freqs <- p.crossover_freqs[1:p.num_bands] + p.band_widths/2
     f <- p.crossover_freqs
   }
   else {print('Invalid type')}
+  return(f)
 }
 
 FFT_band_bins <- function(num_bands){
@@ -131,4 +173,5 @@ FFT_band_bins <- function(num_bands){
   else if(num_bands == 2)  widths <- c(7,   55) # 1+ 1 =  2
   else if(num_bands == 1)  widths <- 62  #1
   #else print('Illegal number of bands')
+  return(widths)
 }
